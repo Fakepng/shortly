7dc9ff66a4359bd4f8af739e4b55f9284874890d
Fakepng <krit0kasemtewin@gmail.com>
2022-11-27 13:09:25 +0700

    Add: new user guide alart

--- pages/link/index.jsx
+++ pages/link/index.jsx
@@ -12,313 +12,325 @@ import Accordion from "react-bootstrap/Accordion";
 import Form from "react-bootstrap/Form";
 import Alert from "react-bootstrap/Alert";
 import {
-	FacebookShareButton,
-	FacebookIcon,
-	LineShareButton,
-	LineIcon,
-	RedditShareButton,
-	RedditIcon,
-	TwitterShareButton,
-	TwitterIcon,
-	FacebookMessengerShareButton,
-	FacebookMessengerIcon,
+  FacebookShareButton,
+  FacebookIcon,
+  LineShareButton,
+  LineIcon,
+  RedditShareButton,
+  RedditIcon,
+  TwitterShareButton,
+  TwitterIcon,
+  FacebookMessengerShareButton,
+  FacebookMessengerIcon,
 } from "next-share";
 import QRCode from "qrcode";
 
 import Header from "../../components/Header";
 
 function Link({ session }) {
-	const [urls, setUrls] = useState([]);
-	const [filteredUrls, setFilteredUrls] = useState([]);
-	const [limit, setLimit] = useState(false);
-	const [show, setShow] = useState(false);
-	const [img, setImg] = useState({});
-	const [search, setSearch] = useState("");
-	const nameRef = useRef(null);
-	const urlRef = useRef("");
-	const router = useRouter();
-	const deferredSearch = useDeferredValue(search);
+  const [urls, setUrls] = useState([]);
+  const [filteredUrls, setFilteredUrls] = useState([]);
+  const [limit, setLimit] = useState(false);
+  const [show, setShow] = useState(false);
+  const [img, setImg] = useState({});
+  const [search, setSearch] = useState("");
+  const nameRef = useRef(null);
+  const urlRef = useRef("");
+  const router = useRouter();
+  const deferredSearch = useDeferredValue(search);
 
-	const handleClose = () => setShow(false);
-	const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
-	const generateQR = async (text, code) => {
-		try {
-			const qr = await QRCode.toDataURL(text);
-			setImg((other) => ({ ...other, [code]: qr }));
-		} catch (err) {
-			console.error(err);
-		}
-	};
+  const generateQR = async (text, code) => {
+    try {
+      const qr = await QRCode.toDataURL(text);
+      setImg((other) => ({ ...other, [code]: qr }));
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-	const resetVisited = async (code) => {
-		await axios.post("/api/resetVisited", { code });
-		setUrls(
-			urls.map((url) => (url.code === code ? { ...url, visited: 0 } : url))
-		);
-	};
+  const resetVisited = async (code) => {
+    await axios.post("/api/resetVisited", { code });
+    setUrls(
+      urls.map((url) => (url.code === code ? { ...url, visited: 0 } : url))
+    );
+  };
 
-	const deleteUrl = async (code) => {
-		await axios.post("/api/deleteUrl", { code });
-		setUrls(urls.filter((url) => url.code !== code));
-	};
+  const deleteUrl = async (code) => {
+    await axios.post("/api/deleteUrl", { code });
+    setUrls(urls.filter((url) => url.code !== code));
+  };
 
-	const urlsMap = filteredUrls.map((url) => {
-		return (
-			<Accordion key={url.code}>
-				<Accordion.Item eventKey={url.code}>
-					<Accordion.Header>
-						{url.name} | {url.code.match(/.{1,4}/g).join("-")} | Visited{" "}
-						{url.visited} times
-					</Accordion.Header>
-					<Accordion.Body>
-						<div>
-							<Button
-								variant='success'
-								type='submit'
-								size='sm'
-								className='bg-green-200'
-								onClick={() => {
-									navigator.clipboard.writeText(
-										`https://shotly.fakepng.com/go/${url.code
-											.match(/.{1,4}/g)
-											.join("-")}`
-									);
-								}}
-							>
-								COPY
-							</Button>{" "}
-							<FacebookShareButton
-								url={`https://shotly.fakepng.com/go/${url.code
-									.match(/.{1,4}/g)
-									.join("-")}`}
-								quote={"Check out this Link!"}
-							>
-								<FacebookIcon size={32} round />
-							</FacebookShareButton>
-							<LineShareButton
-								url={`https://shotly.fakepng.com/go/${url.code
-									.match(/.{1,4}/g)
-									.join("-")}`}
-								title={"Check out this Link!"}
-							>
-								<LineIcon size={32} round />
-							</LineShareButton>
-							<RedditShareButton
-								url={`https://shotly.fakepng.com/go/${url.code
-									.match(/.{1,4}/g)
-									.join("-")}`}
-								title={"Check out this Link!"}
-							>
-								<RedditIcon size={32} round />
-							</RedditShareButton>
-							<TwitterShareButton
-								url={`https://shotly.fakepng.com/go/${url.code
-									.match(/.{1,4}/g)
-									.join("-")}`}
-								title={"Check out this Link!"}
-							>
-								<TwitterIcon size={32} round />
-							</TwitterShareButton>
-							<FacebookMessengerShareButton
-								url={`https://shotly.fakepng.com/go/${url.code
-									.match(/.{1,4}/g)
-									.join("-")}`}
-								appId={process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID}
-							>
-								<FacebookMessengerIcon size={32} round />
-							</FacebookMessengerShareButton>{" "}
-							<Button
-								variant='primary'
-								type='submit'
-								size='sm'
-								className='bg-blue-300'
-								onClick={() => {
-									generateQR(
-										`https://shotly.fakepng.com/go/${url.code
-											.match(/.{1,4}/g)
-											.join("-")}`,
-										url.code
-									);
-								}}
-							>
-								QR
-							</Button>{" "}
-							<Button
-								variant='warning'
-								type='submit'
-								size='sm'
-								className='bg-yellow-100'
-								onClick={() => resetVisited(url.code)}
-							>
-								RESET VISITED
-							</Button>{" "}
-							<Button
-								variant='danger'
-								type='submit'
-								size='sm'
-								className='bg-red-300'
-								onClick={() => deleteUrl(url.code)}
-							>
-								DELETE
-							</Button>
-						</div>
-						{img[url.code] && <img src={img[url.code]} />}
-						<br />
-						{url.url}
-					</Accordion.Body>
-				</Accordion.Item>
-			</Accordion>
-		);
-	});
+  const urlsMap = filteredUrls.map((url) => {
+    return (
+      <Accordion key={url.code}>
+        <Accordion.Item eventKey={url.code}>
+          <Accordion.Header>
+            {url.name} | {url.code.match(/.{1,4}/g).join("-")} | Visited{" "}
+            {url.visited} times
+          </Accordion.Header>
+          <Accordion.Body>
+            <div>
+              <Button
+                variant="success"
+                type="submit"
+                size="sm"
+                className="bg-green-200"
+                onClick={() => {
+                  navigator.clipboard.writeText(
+                    `https://shotly.fakepng.com/go/${url.code
+                      .match(/.{1,4}/g)
+                      .join("-")}`
+                  );
+                }}
+              >
+                COPY
+              </Button>{" "}
+              <FacebookShareButton
+                url={`https://shotly.fakepng.com/go/${url.code
+                  .match(/.{1,4}/g)
+                  .join("-")}`}
+                quote={"Check out this Link!"}
+              >
+                <FacebookIcon size={32} round />
+              </FacebookShareButton>
+              <LineShareButton
+                url={`https://shotly.fakepng.com/go/${url.code
+                  .match(/.{1,4}/g)
+                  .join("-")}`}
+                title={"Check out this Link!"}
+              >
+                <LineIcon size={32} round />
+              </LineShareButton>
+              <RedditShareButton
+                url={`https://shotly.fakepng.com/go/${url.code
+                  .match(/.{1,4}/g)
+                  .join("-")}`}
+                title={"Check out this Link!"}
+              >
+                <RedditIcon size={32} round />
+              </RedditShareButton>
+              <TwitterShareButton
+                url={`https://shotly.fakepng.com/go/${url.code
+                  .match(/.{1,4}/g)
+                  .join("-")}`}
+                title={"Check out this Link!"}
+              >
+                <TwitterIcon size={32} round />
+              </TwitterShareButton>
+              <FacebookMessengerShareButton
+                url={`https://shotly.fakepng.com/go/${url.code
+                  .match(/.{1,4}/g)
+                  .join("-")}`}
+                appId={process.env.NEXT_PUBLIC_FACEBOOK_CLIENT_ID}
+              >
+                <FacebookMessengerIcon size={32} round />
+              </FacebookMessengerShareButton>{" "}
+              <Button
+                variant="primary"
+                type="submit"
+                size="sm"
+                className="bg-blue-300"
+                onClick={() => {
+                  generateQR(
+                    `https://shotly.fakepng.com/go/${url.code
+                      .match(/.{1,4}/g)
+                      .join("-")}`,
+                    url.code
+                  );
+                }}
+              >
+                QR
+              </Button>{" "}
+              <Button
+                variant="warning"
+                type="submit"
+                size="sm"
+                className="bg-yellow-100"
+                onClick={() => resetVisited(url.code)}
+              >
+                RESET VISITED
+              </Button>{" "}
+              <Button
+                variant="danger"
+                type="submit"
+                size="sm"
+                className="bg-red-300"
+                onClick={() => deleteUrl(url.code)}
+              >
+                DELETE
+              </Button>
+            </div>
+            {img[url.code] && <img src={img[url.code]} />}
+            <br />
+            {url.url}
+          </Accordion.Body>
+        </Accordion.Item>
+      </Accordion>
+    );
+  });
 
-	const submitUrl = (event) => {
-		event.preventDefault();
-		if (!nameRef.current.value || !urlRef.current.value) return;
+  const submitUrl = (event) => {
+    event.preventDefault();
+    if (!nameRef.current.value || !urlRef.current.value) return;
 
-		const name = nameRef.current.value;
-		const url = urlRef.current.value;
+    const name = nameRef.current.value;
+    const url = urlRef.current.value;
 
-		axios
-			.post("/api/newUrl", { name, url, id: session.user.id })
-			.then((res) => {
-				if (res.data === "You have reached the limit of 10 urls") {
-					setLimit(() => true);
-				}
-			})
-			.catch((err) => console.log(err));
+    axios
+      .post("/api/newUrl", { name, url, id: session.user.id })
+      .then((res) => {
+        if (res.data === "You have reached the limit of 10 urls") {
+          setLimit(() => true);
+        }
+      })
+      .catch((err) => console.log(err));
 
-		nameRef.current.value = "";
-		urlRef.current.value = "";
-		handleClose();
+    nameRef.current.value = "";
+    urlRef.current.value = "";
+    handleClose();
 
-		if (!limit) {
-			router.reload();
-		}
-	};
+    if (!limit) {
+      router.reload();
+    }
+  };
 
-	useEffect(() => {
-		axios.post("/api/getUser", { id: session.user.id }).then((res) => {
-			setUrls(res.data);
-		});
-	}, []);
+  useEffect(() => {
+    axios.post("/api/getUser", { id: session.user.id }).then((res) => {
+      setUrls(res.data);
+    });
+  }, []);
 
-	useEffect(() => {
-		if (deferredSearch === "") {
-			setFilteredUrls(urls);
-		} else {
-			const filtered = urls.filter((url) => {
-				return (
-					url.name.toLowerCase().includes(deferredSearch.toLowerCase()) ||
-					url.code.toLowerCase().includes(deferredSearch.toLowerCase()) ||
-					url.url.toLowerCase().includes(deferredSearch.toLowerCase()) ||
-					url.code
-						.match(/.{1,4}/g)
-						.join("-")
-						.toLowerCase()
-						.includes(deferredSearch.toLowerCase())
-				);
-			});
+  useEffect(() => {
+    if (deferredSearch === "") {
+      setFilteredUrls(urls);
+    } else {
+      const filtered = urls.filter((url) => {
+        return (
+          url.name.toLowerCase().includes(deferredSearch.toLowerCase()) ||
+          url.code.toLowerCase().includes(deferredSearch.toLowerCase()) ||
+          url.url.toLowerCase().includes(deferredSearch.toLowerCase()) ||
+          url.code
+            .match(/.{1,4}/g)
+            .join("-")
+            .toLowerCase()
+            .includes(deferredSearch.toLowerCase())
+        );
+      });
 
-			setFilteredUrls(filtered);
-		}
-	}, [urls, deferredSearch]);
+      setFilteredUrls(filtered);
+    }
+  }, [urls, deferredSearch]);
 
-	return (
-		<div className='flex flex-col h-screen'>
-			<Head>
-				<title>Link</title>
-				<meta name='description' content='URL Shortener' />
-				<link rel='icon' href='/favicon.ico' />
-			</Head>
-			<header>
-				<Header />
-			</header>
+  return (
+    <div className="flex flex-col h-screen">
+      <Head>
+        <title>Link</title>
+        <meta name="description" content="URL Shortener" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <header>
+        <Header />
+      </header>
 
-			<main className='flex flex-col flex-grow bg-slate-900'>
-				{limit && (
-					<Container>
-						<Alert variant='danger'>
-							<Alert.Heading>
-								You have reached the limit of 10 urls
-							</Alert.Heading>
-						</Alert>
-					</Container>
-				)}
-				<div className='absolute top-4 right-16 mt-20 cursor-pointer md: bottom-0'>
-					<input
-						className='flex ml-2 items-center bg-transparent outline-none text-gray-300 placeholder-gray-400 flex-shrink text-right'
-						type='text'
-						onChange={(event) => setSearch(event.target.value)}
-						placeholder='Search'
-					/>
-				</div>
-				<Icon
-					path={mdiPlusCircle}
-					size={2}
-					color='white'
-					className='absolute top-1 right-0 mt-20 cursor-pointer md:bottom-0'
-					onClick={handleShow}
-				/>
-				<Container>{urlsMap}</Container>
-			</main>
-			<Modal show={show} onHide={handleClose}>
-				<Modal.Header closeButton>
-					<Modal.Title>New URL</Modal.Title>
-				</Modal.Header>
-				<Modal.Body>
-					<Form>
-						<Form.Group className='mb-3' controlId='formBasicName'>
-							<Form.Label>Name:</Form.Label>
-							<Form.Control type='text' placeholder='' ref={nameRef} />
-							<Form.Text className='text-muted'>
-								Name the link as you want to call.
-							</Form.Text>
-						</Form.Group>
+      <main className="flex flex-col flex-grow bg-slate-900">
+        {limit && (
+          <Container>
+            <Alert variant="danger">
+              <Alert.Heading>
+                You have reached the limit of 10 urls
+              </Alert.Heading>
+            </Alert>
+          </Container>
+        )}
+        <div className="absolute top-4 right-16 mt-20 cursor-pointer md: bottom-0">
+          <input
+            className="flex ml-2 items-center bg-transparent outline-none text-gray-300 placeholder-gray-400 flex-shrink text-right"
+            type="text"
+            onChange={(event) => setSearch(event.target.value)}
+            placeholder="Search"
+          />
+        </div>
+        <Icon
+          path={mdiPlusCircle}
+          size={2}
+          color="white"
+          className="absolute top-1 right-0 mt-20 cursor-pointer md:bottom-0"
+          onClick={handleShow}
+        />
+        {urls ? (
+          <Container>
+            <Alert
+              variant="success"
+              onClick={handleShow}
+              className="cursor-pointer text-center"
+            >
+              <Alert.Heading>Create new link here. Click me</Alert.Heading>
+            </Alert>
+          </Container>
+        ) : (
+          <Container>{urlsMap}</Container>
+        )}
+      </main>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>New URL</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicName">
+              <Form.Label>Name:</Form.Label>
+              <Form.Control type="text" placeholder="" ref={nameRef} />
+              <Form.Text className="text-muted">
+                Name the link as you want to call.
+              </Form.Text>
+            </Form.Group>
 
-						<Form.Group className='mb-3' controlId='formBasicLink'>
-							<Form.Label>Url:</Form.Label>
-							<Form.Control type='text' placeholder='https://' ref={urlRef} />
-						</Form.Group>
-						<Button
-							variant='primary'
-							type='submit'
-							onClick={submitUrl}
-							className='bg-blue-300'
-						>
-							Submit
-						</Button>
-					</Form>
-				</Modal.Body>
-				<Modal.Footer>
-					<Button
-						variant='secondary'
-						onClick={handleClose}
-						className='bg-gray-300'
-					>
-						Close
-					</Button>
-				</Modal.Footer>
-			</Modal>
-		</div>
-	);
+            <Form.Group className="mb-3" controlId="formBasicLink">
+              <Form.Label>Url:</Form.Label>
+              <Form.Control type="text" placeholder="https://" ref={urlRef} />
+            </Form.Group>
+            <Button
+              variant="primary"
+              type="submit"
+              onClick={submitUrl}
+              className="bg-blue-300"
+            >
+              Submit
+            </Button>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button
+            variant="secondary"
+            onClick={handleClose}
+            className="bg-gray-300"
+          >
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
 }
 
 export async function getServerSideProps(context) {
-	const session = await getSession(context);
+  const session = await getSession(context);
 
-	if (!session) {
-		return {
-			redirect: {
-				destination: "/",
-				permanent: false,
-			},
-		};
-	}
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
 
-	return {
-		props: { session },
-	};
+  return {
+    props: { session },
+  };
 }
 
 export default Link;
